b0906a019781fca8e09c5c1d7f1a6f7c41550269
Fabiano <fabianoquirinos@outlook.com>
2025-03-13 15:14:35 -0300

    :sparkles: feat: Adição do método remover e adicionar Contas

--- POO/lab-banco-digital-oo/src/model/entities/Banco.java
+++ POO/lab-banco-digital-oo/src/model/entities/Banco.java
@@ -3,11 +3,28 @@ package model.entities;
 import model.interfaces.IConta;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Banco{
     private String nome;
     private List<Conta> contas = new ArrayList<>();
+
+    public void adicionarConta(Conta conta){
+        contas.add(conta);
+    }
+
+    public void removeConta(String nome){
+        Iterator<Conta> iterator = contas.iterator();
+        while (iterator.hasNext()) {
+            Conta c = iterator.next();
+            if (c.getCliente().getNome().equals(nome)) {
+                iterator.remove();
+            }else{
+                throw new IllegalArgumentException("Conta não encontrada e não removida!");
+            }
+        }
+    }
 
     public String getNome() {
         return nome;

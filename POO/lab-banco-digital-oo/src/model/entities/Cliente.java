0b8affd24258cc0aa8333503c0d09d1de1d51d80
Fabiano <fabianoquirinos@outlook.com>
2025-03-13 13:40:18 -0300

    :sparkles: feat: Implementando Classe Cliente

--- POO/lab-banco-digital-oo/src/model/entities/Cliente.java
+++ POO/lab-banco-digital-oo/src/model/entities/Cliente.java
@@ -1,4 +1,13 @@
 package model.entities;
 
 public class Cliente {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }

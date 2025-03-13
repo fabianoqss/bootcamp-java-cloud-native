020b79fefca9511e845ed710e0823666a5368e8a
Fabiano <fabianoquirinos@outlook.com>
2025-03-13 14:37:25 -0300

    :sparkles: feat: Implementação das Informações de Impressão da Conta

--- POO/lab-banco-digital-oo/src/model/entities/ContaPoupanca.java
+++ POO/lab-banco-digital-oo/src/model/entities/ContaPoupanca.java
@@ -1,4 +1,13 @@
 package model.entities;
 
-public class ContaPoupanca {
+public class ContaPoupanca extends Conta{
+    public ContaPoupanca(Integer agencia, Integer numero, Double saldo, Cliente cliente) {
+        super(agencia, numero, saldo, cliente);
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Poupança ===");
+        super.imprimirInfosComuns();
+    }
 }

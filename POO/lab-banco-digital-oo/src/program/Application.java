2a80423a70329d832eea71d61d0a6a2833d456c4
Fabiano <fabianoquirinos@outlook.com>
2025-03-14 13:10:47 -0300

    :sparkles: feat: Adição do método Menu na class Application

--- POO/lab-banco-digital-oo/src/program/Application.java
+++ POO/lab-banco-digital-oo/src/program/Application.java
@@ -21,4 +21,19 @@ public class Application {
             answer = sc.next();
         }while(answer.equalsIgnoreCase("N"));
     }
+
+
+
+    public static void menu(){
+        System.out.println("'---------- MENU ----------'");
+        System.out.println("'Digite Para:'");
+        System.out.println("1 - Abrir Conta ");
+        System.out.println("2 - Remover Conta ");
+        System.out.println("3 - Buscar Conta ");
+        System.out.println("4 - Fazer Operação de Transferência ");
+        System.out.println("5 - Exibir Todas as Contas do Banco ");
+        System.out.println("6 - Para sacar Dinheiro ");
+        System.out.println("7 - Para Depositar dinheiro ");
+        System.out.println("8 - Imprimir extrato de uma Conta");
+    }
 }
